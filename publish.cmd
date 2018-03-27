75953937b7f704a38d186ddc82b914031ba987e2
Sasha Rosenbaum <sasrose@microsoft.com>
2018-03-27 06:40:04 -0400

    my commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=qnabotsasha-Web-Deploy.pubxml -p:Password=YTsbiCGAXBKyxbdG8k1dvJuaoNzJ9CBwRi4GiJCf9TC8NMiqgtfoTeNPqAdL
+
